4a8a500774707da3f0f6e0854456748cb91978fe
Lucien Buttel <lucien.buttel@outlook.com>
2017-09-27 01:11:40 +0200

    splits navlayer in two
    * layer 1: f-keys on the left | arrow, navcluster on the right
    * layer 2: mouse on the left | numpad on the right

--- keyboards/lets_split/keymaps/lbuttel/keymap.c
+++ keyboards/lets_split/keymaps/lbuttel/keymap.c
@@ -9,14 +9,14 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _NAV 1
+#define _NAVPAD 1
+#define _NAVF 2 
 #define _LOWER 3
 #define _RAISE 4
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  NAV, 
   LOWER,
   RAISE,
   ADJUST,
@@ -36,14 +36,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  |      |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | GUI  | Alt  |      |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM(MOD_RSFT) , \
-  KC_LCTL, KC_LALT, KC_LGUI, _______, LOWER, LT(_NAV, KC_SPC), LT(_NAV, KC_ENT),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_ESC       ,  KC_Q  ,    KC_W,    KC_E,  KC_R,                KC_T,              KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,        KC_BSPC, \
+  KC_TAB       ,  KC_A  ,    KC_S,    KC_D,  KC_F,                KC_G,              KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN,        KC_QUOT, \
+  OSM(MOD_LSFT), KC_Z   ,    KC_X,    KC_C,  KC_V,                KC_B,              KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH, OSM(MOD_RSFT) , \
+  KC_LCTL      , KC_LGUI, KC_LALT, _______, LOWER, LT(_NAVPAD, KC_SPC), LT(_NAVF, KC_ENT), RAISE, KC_LEFT, KC_DOWN,   KC_UP,        KC_RGHT \
 ),
 
 /* Nav
@@ -56,12 +56,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------|-----------------------------------------'
- */
+ *
 [_NAV] = KEYMAP( \
-  _______, _______,KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2, _______, _______, KC_PGUP,  KC_UP , KC_PGDN, _______,RALT(KC_Y), \
+  _______, _______  ,KC_MS_BTN1,KC_MS_UP  ,KC_MS_BTN2 , _______, _______, KC_PGUP,  KC_UP , KC_PGDN, _______  ,RALT(KC_Y), \
   KC_DEL ,KC_MS_BTN3,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT, _______, KC_HOME, KC_LEFT, KC_DOWN,KC_RIGHT,RALT(KC_P),RALT(KC_Q), \
-  _______, _______, _______, _______, _______, _______, KC_END , KC_MUTE, KC_VOLD, KC_VOLU,RALT(KC_S), _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  _______, _______  , _______  , _______  , _______   , _______, KC_END , KC_MUTE, KC_VOLD, KC_VOLU,RALT(KC_S), _______, \
+  _______, _______  , _______  , _______  , _______   , _______, _______, _______, _______, _______, _______, _______  \
+),
+*/
+
+/* NavPad
+ * ,-----------------------------------------|-----------------------------------------.
+ * |      |      |MCRigh|  MUp |MCLeft|      |      |   7  |   8  |   9  |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Del  | MCMid| MLeft| MDown|MRight|      |      |   4  |   5  |   6  |   -  |   /  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |   1  |   2  |   3  |   +  |   *  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |   0  |   .  |   ,  |   ^  |   %  |
+ * `-----------------------------------------|-----------------------------------------'
+ */
+[_NAVPAD] = KEYMAP( \
+  _______, _______  ,KC_MS_BTN2,KC_MS_UP  ,KC_MS_BTN1 , _______, _______, KC_7, KC_8   , KC_9   , _______, _______, \
+  KC_DEL ,KC_MS_BTN3,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT, _______, _______, KC_4, KC_5   , KC_6   , KC_MINS, KC_SLSH, \
+  _______, _______  , _______  , _______  , _______   , _______, _______, KC_1, KC_2   , KC_3   , KC_PPLS, KC_PAST, \
+  _______, _______  , _______  , _______  , _______   , _______, _______, KC_0,  KC_DOT, KC_COMM, KC_CIRC, KC_PERC  \
+),
+
+/* NavF
+ * ,-----------------------------------------|-----------------------------------------.
+ * |      |  F10 |  F9  |  F8  |  F7  |      |      | PgUp |  Up  | PgDn |  ü   |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Del  |  F11 |  F6  |  F5  |  F4  |      | Home | Left | Down | Right|  ö   |  ä   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F12 |  F3  |  F2  |  F1  |      |  End | Mute | Vol+ | Vol- |  ß   |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------|-----------------------------------------'
+ */
+[_NAVF] = KEYMAP( \
+  _______,    KC_F10,   KC_F9,   KC_F8,   KC_F7, _______, _______, KC_PGUP,  KC_UP , KC_PGDN,RALT(KC_Y), _______  , \
+  KC_DEL ,    KC_F11,   KC_F6,   KC_F5,   KC_F4, _______, KC_HOME, KC_LEFT, KC_DOWN,KC_RIGHT,RALT(KC_P),RALT(KC_Q), \
+  _______,    KC_F12,   KC_F3,   KC_F2,   KC_F1, _______, KC_END , KC_MUTE, KC_VOLD, KC_VOLU,RALT(KC_S), _______  , \
+  _______, _______  , _______, _______, _______, _______, _______, _______, _______, _______, _______  , _______  \
 ),
 
 /* Nav
@@ -79,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |     |    \  |  |   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |      |   \  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -88,27 +125,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, _______, KC_BSLS, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Raise
- * ,-----------------------------------------------------------------------------------.
+ * ,-----------------------------------------|-----------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
+ * | Del  |   -  |   <  |   [  |   {  |   (  |   )  |   }  |   ]  |   >  |   =  |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------|-----------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
+   KC_DEL, KC_MINS, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK,  KC_EQL, KC_BSLS, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 /* Adjust (Lower + Raise)

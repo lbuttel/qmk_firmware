dcdac83dec65ff9436bb7502e5e859a806fc4b65
Lucien Buttel <lucien.buttel@outlook.com>
2020-02-10 14:59:27 +0100

    adds midi layer

--- keyboards/tada68/keymaps/lbuttel/keymap.c
+++ keyboards/tada68/keymaps/lbuttel/keymap.c
@@ -1,3 +1,4 @@
+#include QMK_KEYBOARD_H
 #include "tada68.h"
 #include "keymap_german.h"
 
@@ -11,8 +12,10 @@
 #define _FL3 3
 #define _GAME 4
 #define _GFN 5
+#define _MIDI 6
 
 #define _______ KC_TRNS
+#define xxxxxxx KC_NO
 
 // tap dance
 #define TD_DOT_COM 0
@@ -51,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Win |Alt |       SpaceFn1        |Alt|Fn3|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_BL] = KEYMAP_ANSI(
+[_BL] = LAYOUT_ansi(
   KC_ESC  ,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,   KC_BSPC, KC_GRV, \
   KC_TAB   ,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
   LT(_FL2,KC_DEL),KC_A,KC_S, KC_D,   KC_F,   KC_G,   KC_H ,  KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,  KC_PGUP, \
@@ -62,19 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
    * |----------------------------------------------------------------|
-   * |     |   |McL|MsU|McR|   |   |PgU| Up|PgD|   | Ü |   |     |Hme |
+   * |     |   |McL|MsU|McR|   |   |PgU| Up|PgD|   | Ü |   |     |    |
    * |----------------------------------------------------------------|
-   * |      |McM|MsL|MsD|MsR|   |Hme|Lef|Dow|Rig| Ö | Ä |        |End |
+   * |      |McM|MsL|MsD|MsR|   |Hme|Lef|Dow|Rig| Ö | Ä |        |MIDI|
    * |----------------------------------------------------------------|
-   * |        |   |   |Bl-|BL |BL+|End|MUT|VU-|VU+| ß |      |   |    |
+   * |        |   |   |Bl-|BL |BL+|End|MUT|VU-|VU+| ß |      |   |GAME|
    * |----------------------------------------------------------------|
    * |    |    |    |                        |   |   |   |   |   |    |
    * `----------------------------------------------------------------'
    */
-[_FL1] = KEYMAP_ANSI(
+[_FL1] = LAYOUT_ansi(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,  KC_INS, \
-  _______  , _______, KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2,_______,_______, KC_PGUP, KC_UP, KC_PGDN, _______,RALT(KC_Y),_______, _______,KC_HOME, \
-  _______    , KC_MS_BTN3, KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,_______,KC_HOME,KC_LEFT,KC_DOWN,KC_RIGHT,RALT(KC_P),RALT(KC_Q),_______,KC_END, \
+  _______  , _______, KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2,_______,_______, KC_PGUP, KC_UP, KC_PGDN, _______,RALT(KC_Y),_______, _______,_______, \
+  _______    , KC_MS_BTN3, KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,_______,KC_HOME,KC_LEFT,KC_DOWN,KC_RIGHT,RALT(KC_P),RALT(KC_Q),_______,TO(_MIDI), \
   _______      , _______, _______, F(BLDN),  BL_TOGG, F(BLUP), KC_END ,  KC_MUTE, KC_VOLD,  KC_VOLU,RALT(KC_S), _______, _______,TO(_GAME), \
   _______, _______, _______,                 _______                                , _______, _______, _______, _______, _______, _______),
 
@@ -92,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                        |   |   |   |   |   |    |
    * `----------------------------------------------------------------'
    */
-[_FL2] = KEYMAP_ANSI(
+[_FL2] = LAYOUT_ansi(
   _______,_______,_______,_______,_______,_______,_______,_______,_______,LSFT(KC_F9),LSFT(KC_F10), _______, _______,     _______, _______, \
   _______  , _______, KC_PERC, KC_SLSH, KC_MINS, KC_PIPE, KC_AMPR, KC_PLUS, KC_ASTR, KC_CIRC, KC_EXLM, KC_QUES, _______,  _______, _______, \
   _______    , _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK,  KC_EQL, _______,         _______, _______, \
@@ -104,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |    |
    * |----------------------------------------------------------------|
-   * |     |   |   |   |   |   |   | 7 | 8 | 9 |  | / |   |     |    |
+   * |     |   |   |   |   |   |   | 7 | 8 | 9 |   | / |   |     |    |
    * |----------------------------------------------------------------|
    * |      |   |   |   |   |   |   | 4 | 5 | 6 | - | * | Enter  |    |
    * |----------------------------------------------------------------|
@@ -113,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |            0           | . |   |   |   |   |    |
    * `----------------------------------------------------------------'
    */
-[_FL3] = KEYMAP_ANSI(
+[_FL3] = LAYOUT_ansi(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, \
   _______  , _______, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, _______, KC_PSLS, _______,  _______, _______, \
   _______    , _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PMNS, KC_PAST,     KC_KP_ENTER, _______, \
@@ -134,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Win |Alt |         Space         |Alt|GFN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_GAME] = KEYMAP_ANSI(
+[_GAME] = LAYOUT_ansi(
   KC_ESC  ,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,   KC_BSPC, KC_GRV, \
   KC_TAB   ,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
   KC_CAPS        ,KC_A,KC_S, KC_D,   KC_F,   KC_G,   KC_H ,  KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,       KC_ENT,  KC_PGUP, \
@@ -155,13 +158,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                        |   |   |   |   |   |    |
    * `----------------------------------------------------------------'
    */
-[_GFN] = KEYMAP_ANSI(
+[_GFN] = LAYOUT_ansi(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12, KC_DEL,  KC_INS, \
   _______  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, \
   _______    , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        _______,  KC_END, \
   _______      , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______,  TO(_BL), \
   _______, _______, _______,                 _______                                , _______, _______, _______, _______, _______, _______),
+
+
+  /* Keymap _MIDI: MIDI Layer
+   * ,----------------------------------------------------------------.
+   * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |    |
+   * |----------------------------------------------------------------|
+   * |     |   |   | C#| D#|   | F#| G#| A#|   |   |   |   |     |    |
+   * |----------------------------------------------------------------|
+   * |      |   | C | D | E | F | G | A | B | C |   |   |        |_BL |
+   * |----------------------------------------------------------------|
+   * |        |   |   |   |   |   |   |   |   |   |   |     |OctU|    |
+   * |----------------------------------------------------------------|
+   * |    |    |    |       Sustain          |   |   |   |  |OctD|    |
+   * `----------------------------------------------------------------'
+   */
+[_MIDI] = LAYOUT_ansi(
+  xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,    xxxxxxx, xxxxxxx, \
+  xxxxxxx  , xxxxxxx, xxxxxxx, MI_Cs_3, MI_Ds_3, xxxxxxx, MI_Fs_3, MI_Gs_3, MI_As_3, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,  xxxxxxx, xxxxxxx, \
+  xxxxxxx    , xxxxxxx, MI_C_3 , MI_D_3 , MI_E_3 , MI_F_3 , MI_G_3 , MI_A_3 , MI_B_3 , MI_C_4 , xxxxxxx, xxxxxxx,         xxxxxxx, TO(_BL), \
+  xxxxxxx      , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx,       xxxxxxx, MI_OCTU, xxxxxxx, \
+  xxxxxxx , xxxxxxx, xxxxxxx,                       MI_SUS                          , xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, MI_OCTD, xxxxxxx),
 };
+
 
 void tap(uint16_t keycode){
   register_code(keycode);
